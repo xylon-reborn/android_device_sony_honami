d9f0940a2fc476688b4d4b361be14a1f5b35e810
xylon-reborn <xylon-reborn@outlook.com>
2013-11-29 10:50:14 +0100

    Omnify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,3 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_honami.mk
+    $(LOCAL_DIR)/full_honami.mk \
+    $(LOCAL_DIR)/omni_honami.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_honami-eng
+add_lunch_combo omni_honami-eng
